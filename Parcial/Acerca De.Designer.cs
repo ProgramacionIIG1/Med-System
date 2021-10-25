debd5125b57d7cb4007acd3f5171c629be5b2339
ERICK MAURICIO TIZNADO RODRIGUEZ <USIS038521@ugb.edu.sv>
2021-10-24 19:00:04 -0600

    Revert "cambios"
    This reverts commit e098cfe14b004377ae50c9dcba3f1e3a7803e027.

--- Parcial/Acerca De.Designer.cs
+++ Parcial/Acerca De.Designer.cs
@@ -54,8 +54,7 @@ namespace Parcial
             this.tableLayoutPanel.Controls.Add(this.textBoxDescription, 1, 4);
             this.tableLayoutPanel.Controls.Add(this.okButton, 1, 5);
             this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel.Location = new System.Drawing.Point(14, 14);
-            this.tableLayoutPanel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tableLayoutPanel.Location = new System.Drawing.Point(9, 9);
             this.tableLayoutPanel.Name = "tableLayoutPanel";
             this.tableLayoutPanel.RowCount = 6;
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
@@ -64,18 +63,17 @@ namespace Parcial
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tableLayoutPanel.Size = new System.Drawing.Size(684, 567);
+            this.tableLayoutPanel.Size = new System.Drawing.Size(457, 369);
             this.tableLayoutPanel.TabIndex = 0;
             // 
             // logoPictureBox
             // 
             this.logoPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.logoPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("logoPictureBox.Image")));
-            this.logoPictureBox.Location = new System.Drawing.Point(4, 5);
-            this.logoPictureBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.logoPictureBox.Location = new System.Drawing.Point(3, 3);
             this.logoPictureBox.Name = "logoPictureBox";
             this.tableLayoutPanel.SetRowSpan(this.logoPictureBox, 6);
-            this.logoPictureBox.Size = new System.Drawing.Size(217, 557);
+            this.logoPictureBox.Size = new System.Drawing.Size(144, 363);
             this.logoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.logoPictureBox.TabIndex = 12;
             this.logoPictureBox.TabStop = false;
@@ -84,11 +82,11 @@ namespace Parcial
             // labelProductName
             // 
             this.labelProductName.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.labelProductName.Location = new System.Drawing.Point(234, 0);
-            this.labelProductName.Margin = new System.Windows.Forms.Padding(9, 0, 4, 0);
-            this.labelProductName.MaximumSize = new System.Drawing.Size(0, 26);
+            this.labelProductName.Location = new System.Drawing.Point(156, 0);
+            this.labelProductName.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
+            this.labelProductName.MaximumSize = new System.Drawing.Size(0, 17);
             this.labelProductName.Name = "labelProductName";
-            this.labelProductName.Size = new System.Drawing.Size(446, 26);
+            this.labelProductName.Size = new System.Drawing.Size(298, 17);
             this.labelProductName.TabIndex = 19;
             this.labelProductName.Text = "Nombre de producto";
             this.labelProductName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -96,11 +94,11 @@ namespace Parcial
             // labelVersion
             // 
             this.labelVersion.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.labelVersion.Location = new System.Drawing.Point(234, 56);
-            this.labelVersion.Margin = new System.Windows.Forms.Padding(9, 0, 4, 0);
-            this.labelVersion.MaximumSize = new System.Drawing.Size(0, 26);
+            this.labelVersion.Location = new System.Drawing.Point(156, 36);
+            this.labelVersion.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
+            this.labelVersion.MaximumSize = new System.Drawing.Size(0, 17);
             this.labelVersion.Name = "labelVersion";
-            this.labelVersion.Size = new System.Drawing.Size(446, 26);
+            this.labelVersion.Size = new System.Drawing.Size(298, 17);
             this.labelVersion.TabIndex = 0;
             this.labelVersion.Text = "Versión";
             this.labelVersion.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -108,11 +106,11 @@ namespace Parcial
             // labelCopyright
             // 
             this.labelCopyright.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.labelCopyright.Location = new System.Drawing.Point(234, 112);
-            this.labelCopyright.Margin = new System.Windows.Forms.Padding(9, 0, 4, 0);
-            this.labelCopyright.MaximumSize = new System.Drawing.Size(0, 26);
+            this.labelCopyright.Location = new System.Drawing.Point(156, 72);
+            this.labelCopyright.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
+            this.labelCopyright.MaximumSize = new System.Drawing.Size(0, 17);
             this.labelCopyright.Name = "labelCopyright";
-            this.labelCopyright.Size = new System.Drawing.Size(446, 26);
+            this.labelCopyright.Size = new System.Drawing.Size(298, 17);
             this.labelCopyright.TabIndex = 21;
             this.labelCopyright.Text = "Copyright";
             this.labelCopyright.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -120,11 +118,11 @@ namespace Parcial
             // labelCompanyName
             // 
             this.labelCompanyName.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.labelCompanyName.Location = new System.Drawing.Point(234, 168);
-            this.labelCompanyName.Margin = new System.Windows.Forms.Padding(9, 0, 4, 0);
-            this.labelCompanyName.MaximumSize = new System.Drawing.Size(0, 26);
+            this.labelCompanyName.Location = new System.Drawing.Point(156, 108);
+            this.labelCompanyName.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
+            this.labelCompanyName.MaximumSize = new System.Drawing.Size(0, 17);
             this.labelCompanyName.Name = "labelCompanyName";
-            this.labelCompanyName.Size = new System.Drawing.Size(446, 26);
+            this.labelCompanyName.Size = new System.Drawing.Size(298, 17);
             this.labelCompanyName.TabIndex = 22;
             this.labelCompanyName.Text = "Nombre de la compañía";
             this.labelCompanyName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -133,13 +131,13 @@ namespace Parcial
             // 
             this.textBoxDescription.BackColor = System.Drawing.Color.LavenderBlush;
             this.textBoxDescription.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBoxDescription.Location = new System.Drawing.Point(234, 229);
-            this.textBoxDescription.Margin = new System.Windows.Forms.Padding(9, 5, 4, 5);
+            this.textBoxDescription.Location = new System.Drawing.Point(156, 147);
+            this.textBoxDescription.Margin = new System.Windows.Forms.Padding(6, 3, 3, 3);
             this.textBoxDescription.Multiline = true;
             this.textBoxDescription.Name = "textBoxDescription";
             this.textBoxDescription.ReadOnly = true;
             this.textBoxDescription.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBoxDescription.Size = new System.Drawing.Size(446, 273);
+            this.textBoxDescription.Size = new System.Drawing.Size(298, 178);
             this.textBoxDescription.TabIndex = 23;
             this.textBoxDescription.TabStop = false;
             this.textBoxDescription.Text = "Descripción";
@@ -149,10 +147,9 @@ namespace Parcial
             this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.okButton.BackColor = System.Drawing.Color.White;
             this.okButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.okButton.Location = new System.Drawing.Point(568, 527);
-            this.okButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.okButton.Location = new System.Drawing.Point(379, 343);
             this.okButton.Name = "okButton";
-            this.okButton.Size = new System.Drawing.Size(112, 35);
+            this.okButton.Size = new System.Drawing.Size(75, 23);
             this.okButton.TabIndex = 24;
             this.okButton.Text = "&Aceptar";
             this.okButton.UseVisualStyleBackColor = false;
@@ -160,17 +157,16 @@ namespace Parcial
             // AboutBox1
             // 
             this.AcceptButton = this.okButton;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightSeaGreen;
-            this.ClientSize = new System.Drawing.Size(712, 595);
+            this.ClientSize = new System.Drawing.Size(475, 387);
             this.Controls.Add(this.tableLayoutPanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "AboutBox1";
-            this.Padding = new System.Windows.Forms.Padding(14, 14, 14, 14);
+            this.Padding = new System.Windows.Forms.Padding(9, 9, 9, 9);
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
--- Parcial/Form2.cs
+++ Parcial/Form2.cs
@@ -16,7 +16,7 @@ namespace Parcial
     {
         public string cadenaconxion = "Database=medsystem;Data Source=localhost;User Id=Erick;Password=1452";
         public int usMod;
-
+        public string PacienteId;
         public Form2()
         {
             InitializeComponent();
@@ -61,6 +61,7 @@ namespace Parcial
 
         private void Form2_Load(object sender, EventArgs e)
         {
+            this.WindowState = FormWindowState.Maximized;
             txtNombre.Enabled = false;
             txtApellido.Enabled = false;
             txtTelefono.Enabled = false;
@@ -68,13 +69,21 @@ namespace Parcial
             comboBox1.Enabled = false;
             txtDireccion.Enabled = false;
             textBox1.Enabled = false;
-            textBox2.Enabled = false;
+            textBox3.Enabled = false;
+
 
             try
             {
-                string consulta = "select Nombre,Apellido,FechaNacimiento,Dui,Telefono,Sexo,Estado,Direccion from pacientes ";
-
+                string consulta = "SELECT pacientes.IdPaciente,pacientes.Nombre,pacientes.Apellido,pacientes.FechaNacimiento,pacientes.Dui,pacientes.Telefono,pacientes.Sexo,pacientes.Estado,pacientes.Direccion,tratamientos.TratamientosActuales,tratamientos.TratamientosPasados,tratamientos.FechaReceta FROM pacientes INNER JOIN tratamientos ON pacientes.IdPaciente = tratamientos.IdPaciente";
+                string contar = "SELECT COUNT(pacientes.IdPaciente) AS contar FROM pacientes";
                 MySqlConnection con = new MySqlConnection(cadenaconxion);
+                MySqlCommand com = new MySqlCommand(contar, con);
+                MySqlDataReader dtr;
+                con.Open();
+                dtr = com.ExecuteReader();
+                dtr.Read();
+                conteo.Text = dtr.GetString(0);
+                con.Close();
                 MySqlDataAdapter comando = new MySqlDataAdapter(consulta, con);
                 System.Data.DataSet ds = new System.Data.DataSet();
                 comando.Fill(ds, "medsystem");
@@ -82,9 +91,9 @@ namespace Parcial
                 dataGridView1.DataMember = "medsystem";
             }
 
-            catch
+            catch(MySqlException k)
             {
-
+                MessageBox.Show(k.ToString());
             }
 
 
@@ -93,47 +102,7 @@ namespace Parcial
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MySqlConnection conn = new MySqlConnection(cadenaconxion);
-            conn.Open();
-            if (txtNombre.Text == "" & txtApellido.Text == "" & txtTelefono.Text == "" & txtDireccion.Text == "")
-            {
-                MessageBox.Show("Hay campos vacios");
-            }
-            else
-            {
-                String update = " ELETE FROM `pacientes` WHERE idPacientes ='" + usMod + "'";
-                MySqlCommand sqlCommand = new MySqlCommand(update, conn);
-                sqlCommand.Parameters.AddWithValue("@nm", txtNombre.Text);
-                sqlCommand.Parameters.AddWithValue("@Ap", txtApellido.Text);
-                sqlCommand.Parameters.AddWithValue("@Fn", dateTimePicker1.Value);
-                sqlCommand.Parameters.AddWithValue("@Tl", Convert.ToInt32(txtTelefono.Text));
-                sqlCommand.Parameters.AddWithValue("@Dir", txtDireccion.Text);
-                try
-                {
-                    sqlCommand.ExecuteNonQuery();
-                    MessageBox.Show("Cambios realizados con exito");
-                    string consulta = "select * from pacientes";
-                    MySqlConnection con = new MySqlConnection(cadenaconxion);
-                    MySqlDataAdapter comando = new MySqlDataAdapter(consulta, con);
-                    System.Data.DataSet ds = new System.Data.DataSet();
-                    comando.Fill(ds, "medsystem");
-                    dataGridView1.DataSource = ds;
-                    txtNombre.Enabled = false;
-                    dateTimePicker1.Enabled = false;
-                    txtApellido.Enabled = false;
-                    txtTelefono.Enabled = false;
-                    txtDireccion.Enabled = false;
-                    txtNombre.Clear();
-                    dateTimePicker1.Enabled = false;
-                    txtApellido.Clear();
-                    txtTelefono.Clear();
-                    txtDireccion.Clear();
-                }
-                catch (MySqlException k)
-                {
-                    MessageBox.Show(k.ToString());
-                }
-            }
+
             }
         
 
@@ -146,8 +115,9 @@ namespace Parcial
                     comboBox1.Enabled = true;
                     txtDireccion.Enabled = true;
                     textBox1.Enabled = true;
-                    textBox2.Enabled = true;
-
+                    textBox3.Enabled = true;
+                    label9.Visible = true;
+                    txtCont.Visible = true;
                     BtnAgregar.Visible = true;
                     btnNew.Visible = false;
 
@@ -167,14 +137,16 @@ namespace Parcial
                     comboBox1.Enabled = true;
                     txtDireccion.Enabled = true;
                     textBox1.Enabled = true;
-                    textBox2.Enabled = true;
+            textBox3.Enabled = true;
+            
+
 
                     btnMod.Visible = false;
                     BtnConfirmar.Visible = true;
 
                 }
 
-        private void BtnConfirmar_Click(object sender, EventArgs e)
+        private void btnElim_Click(object sender, EventArgs e)
         {
             MySqlConnection conn = new MySqlConnection(cadenaconxion);
             conn.Open();
@@ -184,13 +156,8 @@ namespace Parcial
             }
             else
             {
-                String update = "UPDATE pacientes SET Nombre=@nm,Apellidos=@Ap,FechaNacimiento=@Fn,Telefono=@Tl,Direccion=@Dir WHERE idPacientes ='" + usMod + "'";
-                MySqlCommand sqlCommand = new MySqlCommand(update, conn);
-                sqlCommand.Parameters.AddWithValue("@nm", txtNombre.Text);
-                sqlCommand.Parameters.AddWithValue("@Ap", txtApellido.Text);
-                sqlCommand.Parameters.AddWithValue("@Fn", dateTimePicker1.Value);
-                sqlCommand.Parameters.AddWithValue("@Tl", Convert.ToInt32(txtTelefono.Text));
-                sqlCommand.Parameters.AddWithValue("@Dir", txtDireccion.Text);
+                String eliminar = " DELETE FROM `pacientes` WHERE IdPaciente ='" + usMod + "'";
+                MySqlCommand sqlCommand = new MySqlCommand(eliminar, conn);
                 try
                 {
                     sqlCommand.ExecuteNonQuery();
@@ -206,24 +173,116 @@ namespace Parcial
                     txtApellido.Enabled = false;
                     txtTelefono.Enabled = false;
                     txtDireccion.Enabled = false;
+                    textBox1.Enabled = false;
+                    textBox3.Enabled = false;
                     txtNombre.Clear();
                     dateTimePicker1.Enabled = false;
                     txtApellido.Clear();
                     txtTelefono.Clear();
                     txtDireccion.Clear();
+                    textBox1.Clear();
+                    textBox3.Clear();
                 }
-
                 catch (MySqlException k)
-
                 {
                     MessageBox.Show(k.ToString());
-              }
+                }
             }
-          }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
 
         }
-     }
-            
-    
-        
+
+        private void btbBuscar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string consulta = "select * from pacientes where IdPaciente ='" + Convert.ToInt32(txtBuscar.Text) + "' ";
+                MySqlConnection con = new MySqlConnection(cadenaconxion);
+                con.Open();
+                MySqlCommand comm = new MySqlCommand(consulta, con);
+                MySqlDataAdapter comando = new MySqlDataAdapter(consulta, con);
+                MySqlDataReader dtr;
+                dtr = comm.ExecuteReader();
+                if (dtr.Read())
+                {
+                    txtNombre.Text = (dtr.GetString(1));
+                    txtApellido.Text = (dtr.GetString(2));
+                    dateTimePicker1.Value = (DateTime)dtr.GetValue(3);
+                    textBox1.Text = (dtr.GetString(4));
+                    txtTelefono.Text = (dtr.GetString(5));
+                    comboBox1.Text = (dtr.GetString(6));
+                    textBox3.Text = (dtr.GetString(7));
+                    txtDireccion.Text = (dtr.GetString(8));
+                    usMod = Convert.ToInt32(txtBuscar.Text);
+                }
+            }
+            catch (MySqlException k)
+            {
+                MessageBox.Show(k.ToString());
+            }
+        }
+
+        private void BtnAgregar_Click(object sender, EventArgs e)
+        {
+            Class1 generarID = new Class1();
+            PacienteId = "001" + Convert.ToString(generarID.genId9());
+            MySqlConnection mySqlConnection = new MySqlConnection(cadenaconxion);
+            string myInsertQuery = "INSERT INTO pacientes (IdPaciente, Nombre, Apellido, FechaNacimiento,Dui, Telefono,Sexo,Estado, Direccion,IdUsuario  ) Value (@idpas, @nom, @ap, @fn,@dui, @tel,@sexo,@Esta, @direc,@IdUs) ";
+            string insertUsuario = "INSERT INTO usuarios (IdUsuario,Nombre,Apellido,Contraseña) value (@IdUs,@nomb,@ape,@Contra)";
+            MySqlCommand mySqlCommand = new MySqlCommand(myInsertQuery, mySqlConnection);
+            MySqlCommand mySqlCommand1 = new MySqlCommand(insertUsuario, mySqlConnection);
+
+            mySqlCommand.Parameters.AddWithValue("@idpas", Convert.ToInt32(PacienteId));
+            mySqlCommand.Parameters.AddWithValue("@nom", txtNombre.Text);
+            mySqlCommand.Parameters.AddWithValue("@ap", txtApellido.Text);
+            mySqlCommand.Parameters.AddWithValue("@fn", dateTimePicker1.Value);
+            mySqlCommand.Parameters.AddWithValue("@dui", textBox1.Text);
+            mySqlCommand.Parameters.AddWithValue("@tel", Convert.ToInt32(txtTelefono.Text));
+            mySqlCommand.Parameters.AddWithValue("@sexo",comboBox1.Text);
+            mySqlCommand.Parameters.AddWithValue("@Esta", textBox3.Text);
+            mySqlCommand.Parameters.AddWithValue("@direc", txtDireccion.Text);
+            mySqlCommand.Parameters.AddWithValue("@IdUs", Convert.ToInt32(PacienteId) );
+            mySqlCommand1.Parameters.AddWithValue("@IdUs",Convert.ToInt32(PacienteId));
+            mySqlCommand1.Parameters.AddWithValue("@nomb", txtNombre.Text);
+            mySqlCommand1.Parameters.AddWithValue("@ape", txtApellido.Text);
+            mySqlCommand1.Parameters.AddWithValue("@Contra",txtCont.Text);
+            mySqlConnection.Open();
+            try
+            {
+                mySqlCommand1.ExecuteNonQuery();
+                mySqlCommand.ExecuteNonQuery();
+                MessageBox.Show("Usuario agregado con exito");
+                BtnAgregar.Visible = false;
+                btnNew.Visible = true;
+                string consulta = "select * from pacientes";
+                MySqlConnection con = new MySqlConnection(cadenaconxion);
+                MySqlDataAdapter comando = new MySqlDataAdapter(consulta, con);
+                System.Data.DataSet ds = new System.Data.DataSet();
+                comando.Fill(ds, "medsystem");
+                dataGridView1.DataSource = ds;
+                dataGridView1.DataMember = "medsystem";
+                txtNombre.Enabled = false;
+                dateTimePicker1.Enabled = false;
+                txtApellido.Enabled = false;
+                txtTelefono.Enabled = false;
+                txtDireccion.Enabled = false;
+                txtNombre.Clear();
+                dateTimePicker1.Enabled = false;
+                txtApellido.Clear();
+                txtTelefono.Clear();
+                txtDireccion.Clear();
+                label9.Visible = false;
+                txtCont.Visible = false;
+            }
+
+            catch (MySqlException k)
+            {
+                MessageBox.Show(k.ToString());
+            }
+        }
+    }
+        }
     
